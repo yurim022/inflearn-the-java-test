be63e9e536e6d8fe7cd98581815617339c89f659
yurim022 <yurim2220@gmail.com>
2023-05-22 03:58:44 +0900

    feat 기본 테스트 클래스 생성

---
+++ src/main/java/me/whiteship/inflearnthejavatest/study/Study.java
@@ -0,0 +1,4 @@
+package me.whiteship.inflearnthejavatest.study;
+
+public class Study {
+}
---
+++ src/test/java/me/whiteship/inflearnthejavatest/study/StudyTest.java
@@ -0,0 +1,43 @@
+package me.whiteship.inflearnthejavatest.study;
+
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudyTest {
+
+    @Test
+    void create() {
+        Study study = new Study();
+        assertNotNull(study);
+        System.out.println("create");
+    }
+
+    @Test
+    void create2() {
+        Study study = new Study();
+        assertNotNull(study);
+        System.out.println("create 2");
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("before all");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("after all");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("before each");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("after each");
+    }
+
+}
