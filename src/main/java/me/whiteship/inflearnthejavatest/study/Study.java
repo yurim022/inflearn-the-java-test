68efc996ee637274c09194f423103387da439fe1
10151688 <yurim2220@gmail.com>
2023-05-23 18:29:40 +0900

    assertAll

--- src/main/java/me/whiteship/inflearnthejavatest/study/Study.java
+++ src/main/java/me/whiteship/inflearnthejavatest/study/Study.java
@@ -13,4 +13,19 @@ public class Study {
     public int getLimit() {
         return limit;
     }
+
+    public Study(int limit) {
+        if (limit < 0) {
+            throw new IllegalArgumentException("참여제한 인원은 0보다 적을 수 없습니다.");
+        }
+        this.limit = limit;
+    }
+
+    public Study() {
+    }
+
+    public Study(StudyStatus status, int limit) {
+        this.status = status;
+        this.limit = limit;
+    }
 }
--- src/test/java/me/whiteship/inflearnthejavatest/study/StudyTest.java
+++ src/test/java/me/whiteship/inflearnthejavatest/study/StudyTest.java
@@ -11,11 +11,38 @@ class StudyTest {
     @Test
     @DisplayName("새로운 스터디를 생성한다.")
     void create_new_study() {
-        Study study = new Study();
+        Study study = new Study(-10);
         assertNotNull(study);
         assertEquals(StudyStatus.DRAFT,study.getStatus(), "스터디를 처음 만들면 상태값이 DRAFT여야 한다.");
+        assertTrue(study.getLimit() > 0, "스터디 최대 참석 인원은 0보다 커야 한다.");
         System.out.println("create");
     }
+
+
+    @Test
+    @DisplayName("여러개의 assert문을 테스트한다.")
+    void create_new_study_test_assertAll() {
+        Study study = new Study(StudyStatus.ENDED,-10);
+
+        assertAll(
+                ()-> assertNotNull(study),
+                () -> assertEquals(StudyStatus.DRAFT,study.getStatus(), "스터디를 처음 만들면 상태값이 DRAFT여야 한다.") ,
+                () -> assertTrue(study.getLimit() > 0, "스터디 최대 참석 인원은 0보다 커야 한다.")
+        );
+        System.out.println("create");
+    }
+
+
+
+    @Test
+    @DisplayName("특정 exception이 발생하는지 테스트하고 싶을 때")
+    void create_new_study_test_assertThrow() {
+
+        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class, () -> new Study(-10));
+        String message = illegalArgumentException.getMessage();
+        assertEquals(message,"참여제한 인원은 0보다 적을 수 없습니다.");
+    }
+
 
     @Test
     void create2() {
